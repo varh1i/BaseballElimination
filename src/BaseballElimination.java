56b4da19cc2601f962c47da51af0a39e006b3fbf
varhb <Balazs.Varhegyi@pega.com>
2019-03-28 21:31:15 +0100

    Read input

--- src/BaseballElimination.java
+++ src/BaseballElimination.java
@@ -1,61 +1,97 @@
 import edu.princeton.cs.algs4.In;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class BaseballElimination {
 
     private int[] wins;
     private int[] losses;
     private int[] remaining;
     private int[][] against;
-    private String[] teams;
+    private Map<String, Integer> teams;
     private int numOfTeams;
 
     // create a baseball division from given filename in format specified below
     public BaseballElimination(String filename) {
         In in = new In(filename);
         this.numOfTeams = Integer.valueOf(in.readLine());
+        this.wins = new int[numOfTeams];
+        this.losses = new int[numOfTeams];
+        this.remaining = new int[numOfTeams];
+        this.against = new int[numOfTeams][numOfTeams];
+        this.teams = new HashMap<>();
+
         for (int i = 0; i < numOfTeams; i++) {
             String teamInfo = in.readLine();
-            System.out.println(teamInfo);
+            String[] tmp = teamInfo.split(" +");
+            teams.put(tmp[0], i);
+            wins[i] = Integer.valueOf(tmp[1]);
+            losses[i] = Integer.valueOf(tmp[2]);
+            remaining[i] = Integer.valueOf(tmp[3]);
+            for (int j = 0; j < numOfTeams ; j++) {
+                against[i][j] = Integer.valueOf(tmp[4+j]);
+            }
         }
     }
 
-    // number of teams
     public int numberOfTeams() {
         return numOfTeams;
     }
 
-    // all teams
     public Iterable<String> teams() {
-        return null;
+        return teams.keySet();
     }
 
-    // number of wins for given team
     public int wins(String team) {
-        return 0;
+        Integer teamIndex = teams.get(team);
+        if (teamIndex == null) {
+            throw new IllegalArgumentException();
+        }
+        return this.wins[teamIndex];
     }
 
-    // number of losses for given team
     public int losses(String team) {
-        return 0;
+        Integer teamIndex = teams.get(team);
+        if (teamIndex == null) {
+            throw new IllegalArgumentException();
+        }
+        return this.losses[teamIndex];
     }
 
-    // number of remaining games for given team
     public int remaining(String team) {
-        return 0;
+        Integer teamIndex = teams.get(team);
+        if (teamIndex == null) {
+            throw new IllegalArgumentException();
+        }
+        return this.remaining[teamIndex];
     }
 
-    // number of remaining games between team1 and team2
     public int against(String team1, String team2) {
-        return 0;
+        Integer team1Index = teams.get(team1);
+        Integer team2Index = teams.get(team2);
+        if (team1Index == null || team2Index == null) {
+            throw new IllegalArgumentException();
+        }
+        return this.against[team1Index][team2Index];
     }
 
     // is given team eliminated?
     public boolean isEliminated(String team) {
+        Integer teamIndex = teams.get(team);
+        if (teamIndex == null) {
+            throw new IllegalArgumentException();
+        }
         return true;
     }
 
     // subset R of teams that eliminates given team; null if not eliminated
     public Iterable<String> certificateOfElimination(String team) {
+        Integer teamIndex = teams.get(team);
+        if (teamIndex == null) {
+            throw new IllegalArgumentException();
+        }
         return null;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
         BaseballElimination be = new BaseballElimination("resources/teams4.txt");
-        int i = be.numberOfTeams();
-        System.out.println("Number of teams: " + i);
+        for (String team : be.teams()) {
+            System.out.print(team + " " + be.wins(team) + " " + be.losses(team) + " " + be.remaining(team));
+            for (String teamAgainst : be.teams()) {
+                System.out.print(" " + be.against(team, teamAgainst));
+            }
+            System.out.println();
+        }
+
     }
 }
